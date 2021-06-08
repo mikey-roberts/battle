63c13c17a8ec6430876cdfa210909f486e306c83
mikey-roberts <mikey_roberts@outlook.com>
2021-06-08 14:48:09 +0100

    split up cat form onto separate erb

--- app.rb
+++ app.rb
@@ -9,8 +9,17 @@ get '/secret' do
   'This is a secret page'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
 end
 
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
