a97035b42749161740532b6518b9161a35396792
mikey-roberts <mikey_roberts@outlook.com>
2021-06-09 16:31:48 +0100

    redirected to a play method. Stores the variable inside session temporarily to move into instance variable

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,17 +1,3 @@
   feature 'Testing infrastructure' do
-#   scenario 'Can run app and check page content' do
-#     visit('/')
-#     expect(page).to have_content 'Testing infrastructure working!'
-#   end
 
-  scenario 'Expect player to fill out their name' do
-    visit('/')
-    fill_in :player_1_name, with: 'John'
-    fill_in :player_2_name, with: 'Mitch'
-    click_button 'Submit'
-
-    save_and_open_page # will save the web page and open the browser to display it
-
-    expect(page).to have_content 'John vs. Mitch'
-  end
 end
